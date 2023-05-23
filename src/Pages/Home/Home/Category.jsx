a3e517901ccb30f44720e67b5ca2526d58d497c6
tufaelkhan <tufaelkhan247@gmail.com>
2023-05-23 05:19:20 -0700

    add rating icons

--- src/Pages/Home/Home/Category.jsx
+++ src/Pages/Home/Home/Category.jsx
@@ -1,7 +1,8 @@
+import { Rating } from "@smastrom/react-rating";
 import { Link } from "react-router-dom";
 
 const Category = ({shop}) => {
-    const { _id, toyName, seller, subCategory, price, quantity, img, semail, rating} = shop
+    const { _id, toyName, seller, price, quantity, img,  rating} = shop
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <h2 className="card-title">{toyName}!</h2>
@@ -11,7 +12,11 @@ const Category = ({shop}) => {
         <div className="card-body items-center text-center">
             <p>Seller Name: {seller}</p>
             <p>Price: {price}</p>
-            <p>Rating: {rating}</p>
+            <p className="flex">Rating:  <Rating
+      style={{ maxWidth: 100 }}
+      value={rating}
+      readOnly
+    />  {rating}</p>
             <p>Avilable Quantity: {quantity}</p>
             <div className="card-actions">
                 <Link to={`/shop/${_id}`}>
--- src/Pages/Home/ToyDetails.jsx
+++ src/Pages/Home/ToyDetails.jsx
@@ -1,14 +1,12 @@
-import { data } from "autoprefixer";
-// import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
 import { useLoaderData } from "react-router-dom";
 
 const ToyDetails = () => {
     const toysData = useLoaderData()
-    const { _id, name, seller, email, category, price, quantity, photo, detail, toyName, subCategory, img, semail, rating , description} = toysData
+    const { _id, name, seller, email, price, quantity, photo, detail, toyName,  img, semail, rating , description} = toysData
     console.log(toysData);
     return (
         <div className="hero min-h-screen bg-base-200">
-            <h2>one Category here  </h2>
             <div className="hero-content flex-col lg:flex-row">
                 <img src={photo || img} className="max-w-sm rounded-lg shadow-2xl" />
                 <div className="ml-10">
@@ -17,7 +15,11 @@ const ToyDetails = () => {
                     <p>Price: {price}</p>
                     <p>Seller Email: {semail || email}</p>
                     <p>Avilable Quantity: {quantity}</p>
-                    <p>Rating: {rating}</p>
+                    <p className="flex">Rating:  <Rating
+      style={{ maxWidth: 100 }}
+      value={rating}
+      readOnly
+    />{rating}</p>
                     <p>Description: {description || detail}</p>
 
                     <button className="btn btn-primary">shop Now</button>
